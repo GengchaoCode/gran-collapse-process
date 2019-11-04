15713347ace46e946baa9811022e5d6ae1c2088d
Gengchao <gengchao712@outlook.com>
2019-11-04 11:14:02 +0800

    Read video frame-by-frame by the main function

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ Author: YANG Gengchao (The University of Hong Kong)
 '''
 import os
 import pickle
-from pathlib import Path
-os.system('cls')    # clear the screen
+import cv2
+os.system('cls')                    # clear the screen
 
 ## Specify the parameters for video processing and free-surface detection
-folderName = Path('D:/Experimental_Results/2019_HKU_PRFluids_columnSizeEffects/T1b_AR0.50W18_GB1.44mm_Li3.0cm_Hi1.50cm_SP80Cw_20190325/')   # folder where the video exists
+folderName = 'D:\\Experimental_Results\\2019_HKU_PRFluids_columnSizeEffects\\T1b_AR0.50W18_GB1.44mm_Li3.0cm_Hi1.50cm_SP80Cw_20190325\\'   # folder where the video exists
 fileName = 'IMG_1694.MOV'           # video name
 saveName = 'input_parameters.dat'   # name for the saved input parameters
 
-if os.path.isfile(folderName/saveName):
+if os.path.isfile(folderName+saveName):
     print('Input parameters have been saved previously, so import them. ')
-    fid = open(folderName/saveName, 'rb')
+    fid = open(folderName+saveName, 'rb')
     input_list = pickle.load(fid)
     fid.close()
 else:
@@ -33,13 +33,36 @@ else:
     newVidFrameRate = 12            # unit: FPS
     frameStart = 1                  # first frame, set to 1 if not sure
     frameEnd = frameStart+60        # final frame, set to 1 if not sure
+    newVidName = 'video_calibrated.avi' # name of the output video
 
     # save the input parameters
     input_list = [caliBoxHeight, caliBoxLength,
                   cropVidHeight, cropVidLength, axisInterval,
                   newVidFrameRate, frameStart, frameEnd]
-    fid = open(folderName/saveName, 'wb')
+    fid = open(folderName+saveName, 'wb')
     pickle.dump(input_list, fid)
     fid.close()
 
-print(input_list)
+## Capture the video and create the video object for output
+rawVid = cv2.VideoCapture(folderName+fileName)
+
+if not rawVid.isOpened():
+    print('Error capturing the video. Check the folder name and file name.')
+else:
+    while(True):                    # cycle through all the frames
+        ret, frame = rawVid.read()  # read frame-by-frame, note unpacking the tuple here
+
+        # convert to the grey scale images
+        frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # display the frame
+        cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+        cv2.imshow('frame', frameGray)
+        cv2.resizeWindow('frame', 960, 540) # shrink the resolution do show the whole frame
+
+        # wait key input to slow down the video
+        if cv2.waitKey(1) & 0xFF == 27:     # increase the wait time in millisecond to slow down the play
+            break
+
+rawVid.release()                    # release the video after processing
+cv2.destroyAllWindows()             # close all the windows
